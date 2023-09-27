09cca0b169ac44cf628648854f01ccab6eeb0835
skrcka <krcek.adam@gmail.com>
2023-09-27 13:27:00 +0200

    par fix

--- cv_1/src/main.rs
+++ cv_1/src/main.rs
@@ -1,4 +1,4 @@
-use rayon::prelude::{IntoParallelIterator, ParallelIterator};
+use rayon::prelude::{IntoParallelIterator, ParallelBridge, ParallelIterator};
 use rayon_hash::HashMap;
 use std::fs::{read_to_string, write};
 
@@ -157,21 +157,24 @@ fn main() {
 
     // Get clustering effect par
     let sparse_matrix_copy = sparse_matrix.clone();
-    let sparse_matrix_copy2 = sparse_matrix.clone();
-    let sparse_matrix_copy2_arc = std::sync::Arc::new(std::sync::Mutex::new(&sparse_matrix_copy2));
+    let sparse_matrix_arc = std::sync::Arc::new(sparse_matrix_copy);
     let start = std::time::Instant::now();
-    let sum: usize = sparse_matrix_copy
-        .into_par_iter()
-        .map(|(_, v)| {
-            sparse_matrix_copy2_arc
-                .lock()
-                .unwrap()
-                .iter()
-                .filter(|(k, _)| v.contains_key(k))
+    let sum: usize = sparse_matrix_arc
+        .iter()
+        .par_bridge()
+        .map(|(_, neighbors)| {
+            let matrix_ref = sparse_matrix_arc.as_ref();
+            neighbors
+                .keys()
+                .filter_map(|&neighbor| matrix_ref.get(&neighbor))
+                .flatten()
+                .filter(|&(key, _)| neighbors.contains_key(key))
                 .count()
         })
         .sum();
-    let clustering_effect = sum as f64 / sparse_matrix.len() as f64;
+
+    let clustering_effect = sum as f64 / sparse_matrix_arc.len() as f64;
+
     let end = std::time::Instant::now();
     println!(
         "Clustering effect par: {} in {}",
@@ -180,17 +183,17 @@ fn main() {
     );
 
     // Get clustering distribution
+    let sparse_matrix_copy = sparse_matrix.clone();
     let start = std::time::Instant::now();
     let mut clustering_distribution: HashMap<usize, usize> = HashMap::new();
-    for (_, neighbors) in sparse_matrix.iter() {
+    for (_, neighbors) in &sparse_matrix_copy {
         let mut count = 0;
-        for neighbor in neighbors.keys() {
-            if !sparse_matrix.contains_key(neighbor) {
-                continue;
-            }
-            for neighbor_neighbor in sparse_matrix.get(neighbor).unwrap().keys() {
-                if neighbors.contains_key(neighbor_neighbor) {
-                    count += 1;
+        for &neighbor in neighbors.keys() {
+            if let Some(neighbor_neighbors) = sparse_matrix.get(&neighbor) {
+                for &neighbor_neighbor in neighbor_neighbors.keys() {
+                    if neighbors.contains_key(&neighbor_neighbor) {
+                        count += 1;
+                    }
                 }
             }
         }
@@ -203,27 +206,41 @@ fn main() {
     println!("Clustering distribution in {}", (end - start).as_millis());
 
     // Get clustering distribution par
-    let sparse_matrix_copy = sparse_matrix.clone();
-    let sparse_matrix_copy2 = sparse_matrix.clone();
-    let sparse_matrix_copy2_arc = std::sync::Arc::new(std::sync::Mutex::new(&sparse_matrix_copy2));
     let start = std::time::Instant::now();
     let mut clustering_distribution: HashMap<usize, usize> = HashMap::new();
-    let clustering_distribution_arc =
-        std::sync::Arc::new(std::sync::Mutex::new(&mut clustering_distribution));
-    sparse_matrix_copy.into_par_iter().for_each(|(_, v)| {
-        let count = sparse_matrix_copy2_arc
-            .lock()
-            .unwrap()
-            .iter()
-            .filter(|(k, _)| v.contains_key(k))
-            .count();
-        clustering_distribution_arc
-            .lock()
-            .unwrap()
-            .entry(count)
-            .and_modify(|e| *e += 1)
-            .or_insert(1);
-    });
+    let sparse_matrix_copy = sparse_matrix.clone();
+    let results: Vec<HashMap<usize, usize>> = sparse_matrix_copy
+        .into_par_iter()
+        .map(|(_, neighbors)| {
+            let mut local_distribution: HashMap<usize, usize> = HashMap::new();
+            let mut count = 0;
+            for &neighbor in neighbors.keys() {
+                if let Some(neighbor_neighbors) = sparse_matrix.get(&neighbor) {
+                    for &neighbor_neighbor in neighbor_neighbors.keys() {
+                        if neighbors.contains_key(&neighbor_neighbor) {
+                            count += 1;
+                        }
+                    }
+                }
+            }
+            local_distribution
+                .entry(count)
+                .and_modify(|e| *e += 1)
+                .or_insert(1);
+
+            local_distribution
+        })
+        .collect();
+
+    for local_dist in results.iter() {
+        for (&k, &v) in local_dist.iter() {
+            clustering_distribution
+                .entry(k)
+                .and_modify(|e| *e += v)
+                .or_insert(v);
+        }
+    }
+
     let end = std::time::Instant::now();
     println!(
         "Clustering distribution par in {}",
@@ -339,12 +356,18 @@ fn main() {
 }
 
 /*
-Average degree par: 7.9715676988139865 in 77
-Average degree: 7.9715676988139865 in 7
-Max degree: 28576 in 10
-Degree distribution in 203
-Clustering effect: 8.155038221913898 in 83469
-Clustering distribution in 84089
-Average common neighbors: 8.155038221913898 in 82540
-Maximum common neighbors: 152258 in 82767
+Average degree: 7.9715676988139865 in 1
+Average degree par: 7.9715676988139865 in 122
+Max degree: 28576 in 1
+Max degree par: 28576 in 92
+Degree distribution in 8
+Degree distribution par in 141
+Clustering effect: 8.155038221913898 in 4829
+Clustering effect par: 8.155038221913898 in 809
+Clustering distribution in 5080
+Clustering distribution par in 1074
+Average common neighbors: 8.155038221913898 in 4993
+Average common neighbors par: 8.155038221913898 in 885
+Maximum common neighbors: 152258 in 4989
+Maximum common neighbors par: 152258 in 725
 */
