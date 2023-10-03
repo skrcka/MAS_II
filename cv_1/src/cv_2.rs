c5540889d091cf80a08c8298d84676f7a4af4211
skrcka <krcek.adam@gmail.com>
2023-10-03 09:45:36 +0200

    cv_2

--- cv_1/src/cv_2.rs
+++ cv_1/src/cv_2.rs
@@ -48,6 +48,95 @@ fn parse_author_file(
     result
 }
 
+fn compute_degrees_and_weighted_clustering(
+    data: &HashMap<u32, HashMap<usize, HashMap<usize, usize>>>,
+) -> HashMap<u32, (f64, f64, f64)> {
+    let mut result: HashMap<u32, (f64, f64, f64)> = HashMap::new();
+
+    for (&year, matrix) in data {
+        let mut total_degree = 0;
+        let mut total_weighted_degree = 0;
+
+        for author_links in matrix.values() {
+            let degree = author_links.len();
+            let weighted_degree: usize = author_links.values().sum();
+            total_degree += degree;
+            total_weighted_degree += weighted_degree;
+        }
+
+        let avg_degree = total_degree as f64 / matrix.len() as f64;
+        let avg_weighted_degree = total_weighted_degree as f64 / matrix.len() as f64;
+        let weighted_clustering_coeff = compute_weighted_clustering_coefficient(matrix);
+
+        result.insert(
+            year,
+            (avg_degree, avg_weighted_degree, weighted_clustering_coeff),
+        );
+    }
+
+    result
+}
+
+fn compute_weighted_clustering_coefficient(matrix: &HashMap<usize, HashMap<usize, usize>>) -> f64 {
+    let mut total_coefficient = 0.0;
+
+    let w_max = matrix
+        .values()
+        .flat_map(|v| v.values())
+        .cloned()
+        .max()
+        .unwrap_or(1) as f64;
+
+    for (&node, neighbors) in matrix {
+        let k = neighbors.len() as f64;
+
+        if k <= 1.0 {
+            continue; // The clustering coefficient is 0 for k=1
+        }
+
+        let mut sum_weights = 0.0;
+
+        for (&neighbor1, &weight1) in neighbors {
+            if !matrix.contains_key(&neighbor1) {
+                continue;
+            }
+            for (&neighbor2, &weight2) in neighbors {
+                if neighbor1 != neighbor2
+                    && matrix.get(&neighbor1).unwrap().contains_key(&neighbor2)
+                {
+                    sum_weights += (weight1 as f64 + weight2 as f64);
+                }
+            }
+        }
+
+        let c_w = sum_weights / (2.0 * k * (k - 1.0) * w_max);
+        total_coefficient += c_w;
+    }
+
+    total_coefficient / matrix.len() as f64
+}
+
+fn highest_avg_weight_simplex(
+    data: &HashMap<u32, HashMap<usize, HashMap<usize, usize>>>,
+) -> (usize, usize, f64) {
+    let mut max_avg_weight = 0.0;
+    let mut max_simplex = (0, 0);
+
+    for matrix in data.values() {
+        for (&author1, links) in matrix {
+            let total_weight: usize = links.values().sum();
+            let avg_weight = total_weight as f64 / links.len() as f64;
+
+            if avg_weight > max_avg_weight {
+                max_avg_weight = avg_weight;
+                max_simplex = (author1, *links.keys().next().unwrap_or(&0));
+            }
+        }
+    }
+
+    (max_simplex.0, max_simplex.1, max_avg_weight)
+}
+
 pub fn cv_2() {
     let mut sparse_matrices: HashMap<u32, HashMap<usize, HashMap<usize, usize>>> =
         parse_author_file(
@@ -55,4 +144,7 @@ pub fn cv_2() {
             "coauth-DBLP-simplices.txt",
             "coauth-DBLP-times.txt",
         );
+
+    let degrees_over_time = compute_degrees_and_weighted_clustering(&sparse_matrices);
+    let (author1, author2, avg_weight) = highest_avg_weight_simplex(&sparse_matrices);
 }
