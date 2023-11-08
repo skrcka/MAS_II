79d3727f72ad7607f7441031ffb27a37b2fcdaf8
skrcka <krcek.adam@gmail.com>
2023-11-08 14:57:52 +0100

    cv_7

---
+++ cv_1/cv_7.py
@@ -0,0 +1,42 @@
+import random
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+def simulate_influence_spread(
+    graph, *, initial_nodes: list[str] | str | None = None, steps: int = 500, probability: int = 0.5
+):
+    if initial_nodes is None:
+        initial_nodes = random.choice(list(graph.nodes()))
+    if not isinstance(initial_nodes, list):
+        initial_nodes = [initial_nodes]
+
+    influenced_nodes = set(initial_nodes)
+
+    for _ in range(steps):
+        new_influences = set()
+        for node in influenced_nodes:
+            for neighbor in graph.neighbors(node):
+                if random.random() < probability:
+                    new_influences.add(neighbor)
+
+        influenced_nodes.update(new_influences)
+        yield influenced_nodes
+
+
+def main():
+    G = nx.les_miserables_graph()
+
+    for step, influenced in enumerate(simulate_influence_spread(G, steps=10)):
+        plt.figure()
+        nx.draw(
+            G, with_labels=True, font_size=5, node_color=['blue' if node in influenced else 'red' for node in G.nodes()]
+        )
+        #plt.savefig(f"step_{step}.png")  # Saves the figure to a file
+        #plt.close()
+        plt.title(f"Step {step}")
+        plt.show()
+
+
+if __name__ == '__main__':
+    main()
