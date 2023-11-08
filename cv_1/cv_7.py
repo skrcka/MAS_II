7cbd4f193fcf64553b3df75d99829eaed0fc37b7
skrcka <krcek.adam@gmail.com>
2023-11-08 15:26:10 +0100

    done cv_7

--- cv_1/cv_7.py
+++ cv_1/cv_7.py
@@ -11,6 +11,7 @@ def simulate_influence_spread(
     if not isinstance(initial_nodes, list):
         initial_nodes = [initial_nodes]
 
+    inicial_nodes = set(initial_nodes)
     influencing_nodes = set(initial_nodes)
     influenced_nodes = set()
 
@@ -25,33 +26,39 @@ def simulate_influence_spread(
         influenced_nodes.update(influencing_nodes)
         influencing_nodes = new_influences
 
-        yield influencing_nodes, influenced_nodes
+        yield inicial_nodes, influencing_nodes, influenced_nodes
 
 
 def main() -> None:
-    G = nx.les_miserables_graph()
+    graphs = []
+    graphs.append(nx.les_miserables_graph())
+    graphs.append(nx.read_adjlist('socfb-Penn94.txt'))
+    print('Graphs loaded')
 
-    for step, (influencing, influenced) in enumerate(simulate_influence_spread(G, steps=10)):
-        plt.figure()
-        node_colors = []
-        for node in G.nodes():
-            if node in influencing:
-                node_colors.append('red')
-            elif node in influenced:
-                node_colors.append('green')
-            else:
-                node_colors.append('blue')
-        nx.draw(
-            G,
-            pos=nx.spring_layout(G, seed=1),
-            with_labels=True,
-            font_size=5,
-            node_color=node_colors,
-        )
-        #plt.savefig(f"step_{step}.png")  # Saves the figure to a file
-        #plt.close()
-        plt.title(f"Step {step}")
-        plt.show()
+    for i, G in enumerate(graphs):
+        for step, (inicial, influencing, influenced) in enumerate(simulate_influence_spread(G, steps=10)):
+            plt.figure()
+            node_colors = []
+            for node in G.nodes():
+                if node in influencing:
+                    node_colors.append('red')
+                elif node in inicial:
+                    node_colors.append('orange')
+                elif node in influenced:
+                    node_colors.append('green')
+                else:
+                    node_colors.append('blue')
+            nx.draw(
+                G,
+                pos=nx.spring_layout(G, seed=1),
+                with_labels=True,
+                font_size=5,
+                node_color=node_colors,
+            )
+            plt.savefig(f"visualization_{i}/step_{step}.png")  # Saves the figure to a file
+            plt.close()
+            #plt.title(f"Step {step}")
+            #plt.show()
 
 
 if __name__ == '__main__':
