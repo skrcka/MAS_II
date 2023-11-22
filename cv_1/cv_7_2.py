dd6f21c5e08f9bedb7af2b47dbf9b3a3b6ad6f3b
skrcka <krcek.adam@gmail.com>
2023-11-22 15:05:47 +0100

    fix cv_7_2

--- cv_1/cv_7_2.py
+++ cv_1/cv_7_2.py
@@ -2,8 +2,9 @@ import random
 import networkx as nx
 import matplotlib.pyplot as plt
 
+
 def simulate_influence_spread(
-    graph: nx.Graph, *, initial_nodes=None, steps: int = 500, probability: float = 0.5
+    graph: nx.Graph, *, initial_nodes=None, steps: int = 500, probability: float = 0.01
 ):
     if initial_nodes is None:
         initial_nodes = random.choice(list(graph.nodes()))
@@ -26,15 +27,17 @@ def simulate_influence_spread(
 
         yield initial_nodes, influencing_nodes, influenced_nodes
 
-def run_simulation(graph: nx.Graph, initial_nodes, probability: float, steps: int) -> float:
+
+def run_simulation(graph: nx.Graph, initial_nodes: list, probability: float, steps: int) -> float:
     influenced_nodes = set()
-    for _, _, influenced in simulate_influence_spread(
+    for _,influencing, influenced in simulate_influence_spread(
         graph, initial_nodes=initial_nodes, probability=probability, steps=steps
     ):
         influenced_nodes.update(influenced)
+        influenced_nodes.update(influencing)
 
-    # Return the proportion of influenced nodes
     return len(influenced_nodes) / len(graph.nodes())
+
 
 def select_initial_nodes(graph: nx.Graph, num_nodes: int = 5) -> list:
     degree_centrality = nx.degree_centrality(graph)
@@ -49,26 +52,35 @@ def select_initial_nodes(graph: nx.Graph, num_nodes: int = 5) -> list:
 
     return selected_nodes
 
-def run_multiple_simulations(graph: nx.Graph, num_simulations: int = 10, steps: int = 500, probability: float = 0.01):
+
+def run_multiple_simulations(graph: nx.Graph,
+                             num_simulations: int = 10,
+                             node_count = 5,
+                             steps: int = 500,
+                             probability: float = 0.01):
     simulation_results = []
 
     for _ in range(num_simulations):
-        initial_nodes = select_initial_nodes(graph, num_nodes=5)
+        initial_nodes = select_initial_nodes(graph, num_nodes=node_count)
         influenced_rate = run_simulation(graph, initial_nodes, probability, steps)
         simulation_results.append(influenced_rate)
 
     return simulation_results
 
-def main():
-    random.seed(1)
-    graph = nx.read_adjlist('socfb-Penn94.txt')
 
-    simulation_results = run_multiple_simulations(graph, num_simulations=10)
+def main():
+    random.seed(420)
+    graph = nx.read_adjlist('socfb-Penn94.txt')
+    num_simulations = 20
+    node_count = 2
+
+    simulation_results = run_multiple_simulations(graph, node_count=node_count, num_simulations=num_simulations)
 
     plt.boxplot(simulation_results)
-    plt.title('Influence Spread Over Network (10 Simulations)')
+    plt.title(f'Influence Spread Over Network ({num_simulations} Simulations for {node_count} initial nodes)')
     plt.ylabel('Proportion of Influenced Nodes')
     plt.show()
+
 
 if __name__ == '__main__':
     main()
